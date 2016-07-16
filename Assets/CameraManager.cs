bcddc77b5be3532b036bedd2064dd175fdb2cf8f
yamagami kenichi <kenichi-yamagami@altplus.co.jp>
2016-07-16 21:46:12 +0900

    dummy sound

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	public Camera arCam;
+	public Transform bgPanel;
+	// Use this for initialization
+	void Start () {
+		Invoke ("InitCam", 2);
+	}
+
+	void InitCam ()
+	{
+		arCam.farClipPlane = 4000;
+		bgPanel.localPosition = new Vector3 (0, 0, 3000);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

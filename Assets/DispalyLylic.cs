a95ccda2a2257efebfe0ff2ecf5c801f9bcc3f36
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2016-07-16 16:27:30 +0900

    Try to implement a logic to display lylics.

---
+++ Assets/DispalyLylic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DispalyLylic : MonoBehaviour {
+
+    public GameObject characterPrefab;
+    public GameObject[] markers;
+    private Vector3[] characterPositons;
+
+    public string lylic = "Hello, world!";
+
+	// Use this for initialization
+	void Start () {
+        var difference = markers[1].transform.position - markers[0].transform.position;
+        var distance = difference.magnitude;
+        var lengthOfOneSection = distance / (lylic.Length + 1);
+        var direction = difference.normalized;
+
+        characterPositons = new Vector3[lylic.Length];
+        for(int i = 0; i < characterPositons.Length; ++i)
+        {
+            characterPositons[i] = direction * (i + 1) * lengthOfOneSection + markers[0].transform.position;
+            Instantiate(characterPrefab, characterPositons[i], Quaternion.identity);
+            //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //cube.transform.position = characterPositons[i];
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

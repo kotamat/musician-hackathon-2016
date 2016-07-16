d9043208e3f2e17bc70ee3e582283ee00b58fca9
yamagami kenichi <kenichi-yamagami@altplus.co.jp>
2016-07-16 23:00:58 +0900

    clap

--- Assets/Project/ClapController.cs
+++ Assets/Project/ClapController.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class ClapController : MonoBehaviour {
 
+
+	public GameObject pink;
+	public GameObject black;
+
+
+	public int blick = 5;
+
+	private bool isPink = true;
+	private int count;
 	// Use this for initialization
 	void Start () {
-	
+		count = blick;
 	}
-	
+
+	public void StartClap ()
+	{
+		gameObject.SetActive (true);
+	}
+
+
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (count-- < 1) {
+			count = blick;
+			SwitchIcon ();
+		}
+	}
+
+	void SwitchIcon ()
+	{
+		isPink = !isPink;
+		pink.SetActive (isPink);
+		black.SetActive (!isPink);
 	}
 }

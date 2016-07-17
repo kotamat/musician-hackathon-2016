42285663ec6236d2d641d939053c47c6708cba6a
yamagami kenichi <kenichi-yamagami@altplus.co.jp>
2016-07-17 11:25:16 +0900

    SoundSwitcher

---
+++ Assets/SceneController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+public class SceneController : MonoBehaviour {
+
+	static private SceneController _instance;
+	static public SceneController Instance {
+		get { return _instance;}
+	}
+
+	public AudioClip [] audioClips;
+	public AudioSource audioSource;
+	public ClapController clap;
+	public int clapWait = 3;
+	private Sequence clapWaiter;
+
+	// Use this for initialization
+	void Start () {
+		_instance = this;
+		//PlaySound ();
+
+	}
+
+	public void PlaySound (int index=0)
+	{
+		if (index == -1) {
+			StopAllSound ();
+		}
+		audioSource.clip = audioClips [index];
+		audioSource.Play ();
+
+		clapWaiter = new Sequence ();
+		clapWaiter.AppendInterval (clapWait);
+		clapWaiter.AppendCallback (clap.StartClap);
+		clapWaiter.Play ();
+
+	}
+	public void StopAllSound ()
+	{
+		audioSource.Stop ();
+		clapWaiter.Kill ();
+	}
+
+}
